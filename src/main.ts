701c4d46f74405a0d4781acd4702039401faaa9c
Mohamed Sayed <mohamedelsayed0901@gmailcom>
2024-09-27 20:39:25 +0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
